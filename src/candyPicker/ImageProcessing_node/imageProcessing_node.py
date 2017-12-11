d08b3a9810c04312d94935bfc95be088224c017c
ubuntu_12.04_ROS <ubuntu@ubuntu.(none)>
2017-12-11 07:05:09 -0800

    dd

--- src/candyPicker/ImageProcessing_node/imageProcessing_node.py
+++ src/candyPicker/ImageProcessing_node/imageProcessing_node.py
@@ -19,7 +19,8 @@ class imageProcessing_node():
     def callbackSort(self, message):
         #if (self.AllowedToProcessImage == True):
         print "imageProc callback function"
-        self.MMsPixelCord_publisher.publish([10,10,10])
+        arr = [10,10,10]
+        self.MMsPixelCord_publisher.publish(arr)
             
     def callbackDoneMoving(info, message):
         self.AllowedToProcessImage = True
